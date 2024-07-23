c92e5e1a8b7dedbba8e09c88331046a26e9d18fc
Dell <Dell@192.168.1.39>
2024-07-23 19:46:13 +0530

    day 1 code

---
+++ ProjectTNSjava/src/javacode/Day_1.java
@@ -0,0 +1,53 @@
+package javacode;
+
+public class Day_1 {
+
+	public static void main(String [] args) {
+		System.out.println("hello world");
+		int a=10;
+		int b=20;
+		int $sum=a+b;
+		System.out.println("sum is : "+ $sum );
+		//data types------
+		/*
+		 byte-1(byte),
+		 short-2(byte),
+		 int-4(byte),
+		 long-8(byte),
+		 float-4(byte),
+		 char-2(byte),
+		 double-8(byte)
+		 ,boolean-1(bit)
+		 */
+		int val1=5/2;
+		float val2=5;
+		double val3=100d/3d;
+		boolean val4=true;
+		char c='a';
+		
+		System.out.println(val1);
+		System.out.println(val2);
+		System.out.println(val3);
+		System.out.println(c);
+		System.out.println(24);
+		
+		float side1=10f;
+		float areaSqare=side1*side1;
+		System.out.println("area of square is : "+ areaSqare);
+		
+		float radius= 5;
+		double areaofCircle=radius*radius*3.14;
+		System.out.println("area of circle is : "+ areaofCircle);
+		
+		float side2=10;
+		float side3=20;
+		float areaofRectangle=side2*side3;
+		System.out.println("area of Rectangle is : "+ areaofRectangle);
+		
+		
+		
+		
+		
+	}
+
+}
